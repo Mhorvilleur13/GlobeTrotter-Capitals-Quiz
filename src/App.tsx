9aeccf33db585d8a53399f3691f3769a2da78943
Mhorvilleur13 <mhorvilleur13@gmail.com>
2023-03-14 12:02:15 -0600

    added background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+  return <div className="page-container background-image"></div>;
 }
 
 export default App;
