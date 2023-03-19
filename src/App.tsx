427820bca29e1371a5d7fd29499abcf894e2a7bd
Mhorvilleur13 <mhorvilleur13@gmail.com>
2023-03-19 10:48:36 -0600

    added the card and form submti

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import "./App.css";
 import "./index.css";
+import Search from "./Components/Search/search";
+import { Container } from "react-bootstrap";
 
 function App() {
-  return <div className="page-container background-image"></div>;
+  return (
+    <div className="page-container background-image">
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Search />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
