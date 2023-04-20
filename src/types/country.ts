cd79982ae1011855264957c6a8383f312fc7c6b0
Mhorvilleur13 <mhorvilleur13@gmail.com>
2023-04-20 09:44:35 -0600

    globe added and and moves to first country in array

--- src/types/country.ts
+++ src/types/country.ts
@@ -1,9 +1,16 @@
+import { number } from "@amcharts/amcharts4/core";
+
 export interface Countries {
   [x: string]: any;
   [index: number]: { [x: string]: string };
 }
 
+type LatLng = number;
+
 export interface Country {
-  [countryName: string]: Capital[];
+  country: string;
+  capital: Capital[];
+  latlng: LatLng[];
 }
+
 type Capital = string;
